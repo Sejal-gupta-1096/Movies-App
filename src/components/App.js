f30ce08759c1e60be9a10b01005224ee8009e129
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-07-14 02:51:11 -0700

    Style the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 
 function App() {
   return (
     <div className="App">  
-      Hellooo
+      <Navbar />
+      <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourites</div>
+        </div>
+        <div className='list'>
+          {data.map( (movie) => {
+            return <MovieCard movie={movie} />
+          })}
+      </div>
+    </div>
+      
     </div>
   );
 }
