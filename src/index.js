470d7d60ebe99c1c3d7e3f6bc871221addb0e747
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-07-15 08:02:18 -0700

    Install redux and add actions,reducers and store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
